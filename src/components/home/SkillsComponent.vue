8e9761450777a8588c25f06ae6e5584c9db26beb
unknown <goodlucketim94@gmail.com>
2024-06-28 07:59:03 +0100

    bugs

--- src/components/home/SkillsComponent.vue
+++ src/components/home/SkillsComponent.vue
@@ -7,51 +7,51 @@
         <h2 class="text-slate-400 p-4 font-bold text-center text-4xl my-10">MY SKILLS</h2>
         <div class="skills text-white md:w-7/12 mx-auto">
             <div class="w-full bg-slate-100 relative mb-4">
+              <span class="absolute top-0 right-2 z-10 text-slate-900">90%</span>
               <div class="skill-rate bg-slate-400 w-11/12 bottom-0" data-aos="slide-right" data-aos-duration="2000">
                 <p class="text-slate-900 p-1 font-bold">HTML</p>
-                <span class="absolute top-0 right-2 text-slate-900">90%</span>
               </div>  
             </div>
 
             <div class="w-full bg-slate-100 relative mb-4">
+              <span class="absolute top-0 right-2 z-10 text-slate-900">80%</span>
               <div class="skill-rate bg-slate-400 w-[80%] bottom-0" data-aos="slide-right" data-aos-duration="2000">
                 <p class="text-slate-900 p-1 font-bold">CSS</p>
-                <span class="absolute top-0 right-2 text-slate-900">80%</span>
               </div>  
             </div>
 
             <div class="w-full bg-slate-100 relative mb-4" id="js">
+              <span class="absolute top-0 right-2 z-10 text-slate-900">75%</span>
               <div class="skill-rate bg-slate-400 w-[75%] bottom-0" data-aos="slide-right" data-aos-duration="2000">
                 <p class="text-slate-900 p-1 font-bold">Javascript</p>
-                <span class="absolute top-0 right-2 text-slate-900">75%</span>
               </div>  
             </div>
 
             <div class="w-full bg-slate-100 relative mb-4">
+              <span class="absolute top-0 right-2 z-10 text-slate-900">85%</span>
               <div class="skill-rate bg-slate-400 w-[85%] bottom-0" data-aos="slide-right" data-aos-duration="2000">
                 <p class="text-slate-900 p-1 font-bold">Tailwind CSS</p>
-                <span class="absolute top-0 right-2 text-slate-900">85%</span>
               </div>  
             </div>
 
             <div class="w-full bg-slate-100 relative mb-4">
+              <span class="absolute top-0 right-2 z-10 text-slate-900">70%</span>
               <div class="skill-rate bg-slate-400 w-[70%] bottom-0" data-aos="slide-right" data-aos-duration="2000">
                 <p class="text-slate-900 p-1 font-bold">Vue Js</p>
-                <span class="absolute top-0 right-2 text-slate-900">70%</span>
               </div>  
             </div>
 
             <div class="w-full bg-slate-100 relative mb-4">
-              <div class="skill-rate bg-slate-400 w-[30%] bottom-0" data-aos="slide-right" data-aos-duration="2000">
+              <span class="absolute top-0 right-2 z-10 text-slate-900">35%</span>
+              <div class="skill-rate bg-slate-400 w-[35%] bottom-0" data-aos="slide-right" data-aos-duration="2000">
                 <p class="text-slate-900 p-1 font-bold">Node Js</p>
-                <span class="absolute top-0 right-2 text-slate-900">30%</span>
               </div>  
             </div>
 
             <div class="w-full bg-slate-100 relative mb-4">
+              <span class="absolute top-0 right-2 z-10 text-slate-900">50%</span>
               <div class="skill-rate bg-slate-400 w-6/12 bottom-0" data-aos="slide-right" data-aos-duration="2000">
                 <p class="text-slate-900 p-1 font-bold">PHP</p>
-                <span class="absolute top-0 right-2 text-slate-900">50%</span>
               </div>  
             </div>
 
