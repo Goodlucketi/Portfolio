497d1e89fa994407fce8e868fc457b836cf5fa38
unknown <goodlucketim94@gmail.com>
2024-07-06 17:31:08 +0100

    about me

--- src/components/home/AboutComponent.vue
+++ src/components/home/AboutComponent.vue
@@ -12,7 +12,19 @@
             </div>
 
             <div class="about-txt mx-auto text-justify text-slate-600 md:w-8/12" data-aos="fade-left" data-aos-duration="2000">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum sint et quibusdam corporis animi culpa sapiente, explicabo quo error ut iusto, obcaecati alias quis aliquid autem tempora at. Minus fugit id excepturi? Blanditiis impedit ea pariatur debitis earum repellendus recusandae, in laboriosam dolorum ad reprehenderit suscipit possimus quae molestiae inventore autem optio reiciendis id vel corrupti itaque quos rem ipsum? Consequuntur facere quas ullam voluptatem voluptatibus delectus ab illo exercitationem. Quae obcaecati aspernatur officia blanditiis necessitatibus placeat repellendus amet tempore. Ad perferendis cumque libero dolores, distinctio quasi! Suscipit illum explicabo doloribus nulla ipsa sint dolorem aliquid voluptatem. Corporis, quasi consequuntur?</p>
+                <p>
+
+                Hello, I'm Mfoniso Fabian Etim, a passionate and dedicated technology enthusiast from Akwa Ibom State, Nigeria. With a Bachelor's degree in Computer Engineering from the University of Uyo, I have cultivated a deep understanding and proficiency in various web technologies including HTML, CSS, JavaScript, Vue.js, React.js, PHP, and Node.js. <br>
+
+                My professional journey has seen me take on roles that hone my skills and broaden my experience in the tech industry. As a Web Instructor at Classic Systems Infotech Limited in Ado Ekiti, I had the opportunity to share my knowledge and inspire budding developers. My role as a Front End Developer at Kinplus Technologies in Ekiti State allowed me to work on innovative projects, pushing the boundaries of web development. <br>
+
+                Driven by a strong belief in the power of technology to transform lives, I founded Meln Hub in Uyo, Akwa Ibom State. Through this initiative, I aim to create a community where technology enthusiasts can collaborate, learn, and innovate together. <br>
+
+                Beyond my professional endeavors, I have a deep passion for giving back to humanity. I am committed to solving real-world problems through technology and educating others about the vast possibilities available in this field. My hobbies, which include coding, meditating, playing football, gaming, and making friends, keep me balanced and constantly fuel my creativity. <br>
+
+                I believe that technology is a tool for change and empowerment. By leveraging my skills and experience, I strive to make a positive impact on society and help others achieve their purpose.
+
+                </p>
             </div>
         </div>
     </main>
